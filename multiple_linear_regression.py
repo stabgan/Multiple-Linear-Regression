d44f08b8ad0ffe80e98ed9123d98d9a79a49fb27
Kaustabh Ganguly <stabgan@users.noreply.github.com>
2026-03-14 15:02:17 +0530

    fix: modernize code + rewrite README

--- multiple_linear_regression.py
+++ multiple_linear_regression.py
@@ -8,20 +8,23 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('50_Startups.csv')
 X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 4].values
+y = dataset.iloc[:, -1].values
 
 # Encoding categorical data
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder = LabelEncoder()
-X[:, 3] = labelencoder.fit_transform(X[:, 3])
-onehotencoder = OneHotEncoder(categorical_features = [3])
-X = onehotencoder.fit_transform(X).toarray()
+# Modern approach: use ColumnTransformer + OneHotEncoder (LabelEncoder + categorical_features is deprecated)
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(
+    transformers=[('encoder', OneHotEncoder(), [3])],
+    remainder='passthrough'
+)
+X = np.array(ct.fit_transform(X))
 
 # Avoiding the Dummy Variable Trap
 X = X[:, 1:]
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
@@ -41,8 +44,8 @@ regressor.fit(X_train, y_train)
 y_pred = regressor.predict(X_test)
 
 # Building the optimal model using Backward Elimination
-import statsmodels.formula.api as sm
-X = np.append(arr = np.ones((50, 1)).astype(int), values = X, axis = 1)
+import statsmodels.api as sm
+X = np.append(arr = np.ones((X.shape[0], 1)).astype(int), values = X, axis = 1)
 X_opt = X[:, [0, 1, 2, 3, 4, 5]]
 regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
 regressor_OLS.summary()
